2d4c7c890b0eb88fa597233e504a1377a60450a9
rumiarime <148171622+rumiarime@users.noreply.github.com>
2024-01-28 10:46:45 +0900

    PHP_課題007をアップデート

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP 007課題</title>
+</head>
+
+<body>
+     <p>
+         <?php
+         // 連想配列に値を代入する
+         $vegetables_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+ 
+         // 連想配列の値を出力する
+         print_r($vegetables_data);
+ 
+
+         ?>
+     </p>
+</body>
+ 
+</html>
